85d00f54a1f8195f096c5a3213d5afc252765437
= <=>
2023-10-24 14:30:07 +0000

    reverse proxy setup

--- src/modules/ec2/nginx_ssh_config.sh
+++ src/modules/ec2/nginx_ssh_config.sh
@@ -2,17 +2,39 @@
 #Author: michael_kedey
 #Date: 19/10/2023
 #Last_modified: 20/10/2023
-#Purpose: install apache and change the default ssh port from 22 to 273
+#Purpose: install nginx and change the default ssh port
 #Use_case: user file to run on boot after a new ec2 has been instantaited
 
 sudo apt-get update
 sudo apt-get upgrade -y
 
+#install nginx and start the service
 sudo apt-get install -y nginx
 sudo systemctl enable nginx
 sudo systemctl start nginx
 
-echo -e '<h1>Congrats mickey! you have installed nginx</h1>' > var/www/html/index.html
+echo -e '<h1>Congrats! you have installed nginx</h1>' > var/www/html/index.html
+
+# Set up a reverse proxy in NGINX to forward requests to the  local server
+local_server="http://localhost:80"
+sudo rm /etc/nginx/sites-available/default  
+sudo tee /etc/nginx/sites-available/default <<EOF
+server {
+    listen 80;
+
+    location / {
+        proxy_pass $local_server;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+    }
+}
+EOF
+
+# Test the NGINX configuration and restart if it's OK
+sudo nginx -t && sudo systemctl restart nginx
 
 # Change the SSH port to 273
 new_port=273
--- src/providers.tf
+++ src/providers.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    #bucket exists already
+    bucket = "sedem-terra333-bucket"
+    key    = "cicd-git-demo2/terraform.tfstate"
+    region = "us-east-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
